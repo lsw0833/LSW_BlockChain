f63edc3a9cf6c57f2cdc958555671ab81c1894f8
Sangwonlee93 <lsw0833@hanmail.net>
2018-05-15 14:42:54 +0900

    코인 빼고는 완성

--- blockChain.js
+++ blockChain.js
@@ -11,9 +11,11 @@ var nodeName = "julia";
 var isMining = false;
 var blockChain = [];
 var recieveTXID = [];
-var syncLastBlockHash=[];
+var syncLastBlockHash = [];
 var app = express();
-app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 //var errorhandler = expressErrorHandler({
 //  static:{
@@ -22,17 +24,20 @@ app.use(bodyParser.json());
 //});
 //app.use(expressErrorHandler.httpError(404));
 //app.use(errorhandler);
-app.post('/data',(req,res)=>{
+app.post('/data', (req, res) => {
   let txID = req.body.TXID;
   let txData = req.body.TXdata;
-  var newData = {TXID : txID, Txdata : txData};
+  var newData = {
+    TXID: txID,
+    Txdata: txData
+  };
   console.log("***************************");
   console.log("Data : " + newData);
   console.log("***************************");
   data.push(newData);
   console.log(data);
   recieveTXID.push(newData["TXID"]);
-  io.emit("addData",newData);
+  io.emit("addData", newData);
   res.send();
 });
 
@@ -40,35 +45,37 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function deleteinData(list){
-  for(var i=0; i<list.length; i++){
-    for(var j=0; j<data.length; j++){
-      if(data[j]["TXID"]==list[i]["TXID"]){
-        data.splice(j,1);
+function deleteinData(list) {
+  for (var i = 0; i < list.length; i++) {
+    for (var j = 0; j < data.length; j++) {
+      if (data[j]["TXID"] == list[i]["TXID"]) {
+        data.splice(j, 1);
         j--;
       }
     }
   }
 }
-function isinBlock(data1){
-  for(var i=0; i<blockChain.length; i++){
-    if(blockChain[i]["blockHash"]==data1["blockHash"]){
+
+function isinBlock(data1) {
+  for (var i = 0; i < blockChain.length; i++) {
+    if (blockChain[i]["blockHash"] == data1["blockHash"]) {
       return true;
     }
   }
   return false;
 }
+
 function addNode(ip) {
   var socket = require('socket.io-client')('http://' + ip);
   socket.on('findBlock', function(data1) {
-    if(!isinBlock(data1)){
+    if (!isinBlock(data1)) {
       exec("ps -ef | grep MakeBlock | awk '{print $2}' | xargs kill -9", function(err, stdout, stderr) {
         let flag = false;
         // 블록이 유효한지, 유효하지않다면 그 블록 제외하는 법과 그 노드 제외
         var options = {
           env: {
-            "nonce" : data1["nonce"],
-            "merkleHash":  data1["merkleHash"],
+            "nonce": data1["nonce"],
+            "merkleHash": data1["merkleHash"],
             "version": data1["version"],
             "blockHash": data1["blockHash"],
             "time": data1["time"],
@@ -78,23 +85,25 @@ function addNode(ip) {
         };
         exec('python ./CreateBlock/block/CheckBlock.py', options, function(error, stdout, stderr) {
           if (error) {} else {
-            if(stdout.trim() == "true"){
+            if (stdout.trim() == "true") {
               flag = true;
             }
-            if(flag){
-              if(data1["previousBlockHash"] == blockChain[blockChain.length-1]["blockHash"]){
+            if (flag) {
+              if (data1["previousBlockHash"] == blockChain[blockChain.length - 1]["blockHash"]) {
                 let temp = data1["data"];
                 deleteinData(temp);
                 blockChain.push(data1);
                 io.emit("findBlock", data1);
                 console.log("get block from other Node");
                 console.log(data1);
-              }else{
-                  syncLastBlockHash.push(blockChain[blockChain.length-1]["blockHash"]);
-                  io.emit('syncBlockChain',blockChain);
+              } else {
+                if (!isinBlock(data1)) {
+                  syncLastBlockHash.push(blockChain[blockChain.length - 1]["blockHash"]);
+                  io.emit('syncBlockChain', blockChain);
                   console.log("Data synchronized by " + nodeName);
+                }
               }
-            }else{
+            } else {
               // 블록 만든 노드 트랍
               console.log("WTF");
             }
@@ -106,10 +115,10 @@ function addNode(ip) {
   });
   //여기에 전자서명 필요
   socket.on('addData', function(recieveData) {
-    if(recieveTXID.indexOf(recieveData.TXID)==-1){
+    if (recieveTXID.indexOf(recieveData.TXID) == -1) {
       data.push(recieveData);
       recieveTXID.push(recieveData.TXID);
-      io.emit('addData',recieveData);
+      io.emit('addData', recieveData);
       console.log("-------------------------------");
       console.log("Add data");
       console.log(data);
@@ -117,18 +126,18 @@ function addNode(ip) {
     }
   });
   socket.on('connected', function(blockChain1) {
-    if(blockChain.length==0){
+    if (blockChain.length == 0) {
       blockChain = blockChain1;
       console.log("----Connect-----");
       console.log(blockChain);
     }
   });
   socket.on('syncBlockChain', function(blockChain1) {
-    if(syncLastBlockHash.indexOf(blockChain1[blockChain1.length-1]["blockHash"])==-1){
+    if (syncLastBlockHash.indexOf(blockChain1[blockChain1.length - 1]["blockHash"]) == -1) {
       exec("ps -ef | grep MakeBlock | awk '{print $2}' | xargs kill -9", function(err, stdout, stderr) {
         blockChain = blockChain1;
-        syncLastBlockHash.push(blockChain[blockChain.length-1]["blockHash"]);
-        io.emit('syncBlockChain',blockChain);
+        syncLastBlockHash.push(blockChain[blockChain.length - 1]["blockHash"]);
+        io.emit('syncBlockChain', blockChain);
         isMining = false;
       });
     }
@@ -145,9 +154,9 @@ function mining(previous) {
   // 파이썬 코드 실행
   console.log("Run Mining at " + new Date().getTime());
   isMining = true;
-  let datainMining=[];
-  let TXinMining=[];
-  for(var i in data){
+  let datainMining = [];
+  let TXinMining = [];
+  for (var i in data) {
     datainMining.push(data[i].TXID);
     TXinMining.push(data[i]);
   }
@@ -183,7 +192,7 @@ function mining(previous) {
         blockHash: buf[3],
         time: buf[4],
         previousBlockHash: buf[5],
-        bits : buf[6],
+        bits: buf[6],
         data: TXinMining,
         MinerName: nodeName
       };
